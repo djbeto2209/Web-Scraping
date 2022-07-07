488dc53b14cc7b4fbe37e1248a8317c6cb35133a
Roberto.Gonzalez1 <wowroberto80@gmail.com>
2022-07-07 16:18:33 -0400

    testing_BS4

--- web_scraping.py
+++ web_scraping.py
@@ -1,1 +1,12 @@
 from bs4 import BeautifulSoup
+import requests
+import lxml
+
+# This is the URL of the site you want to scrap
+url = 'https://webscraper.io/test-sites/e-commerce/allinone/computers'
+#Pulls HTML data from the URL
+page = requests.get(url)   
+#Cleans up the HTML so it is readable
+soup = BeautifulSoup(page.text, 'lxml')
+
+print(soup)
